ae43fb654fd75e350eef3575341e048c335715f4
Tamás Kovács <87983312+tamaskovacs0408@users.noreply.github.com>
2022-07-19 10:36:09 +0200

    Update scripts.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// Kijelöljük egy változóba a forward-arrow képet
+
 const forwardArrow = document.querySelector(".js-forward");
 const backwardArrow = document.querySelector(".js-backward")
-// Létrehozunk egy függvényt ami az arrow-ra klikkelésre indul el.
+
 forwardArrow.onclick = function() {
-    // Kijelöljük a tárolót (js-movies), amire egy scrollLeft attribútumot adunk meg ami 800px-el tolja el balra (scrollLeft += 800)
+    
     document.querySelector(".js-movies").scrollLeft += 800;
 }
-// U.e visszafele scrollozáshoz a -= 800-al
+
 backwardArrow.onclick = function() {
     document.querySelector(".js-movies").scrollLeft -= 800;
 }
